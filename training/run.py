914a72b72124eb1b489b497ed97a425a8161bfd7
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-26 13:46:20 -0400

    updated run.py

--- training/run.py
+++ training/run.py
@@ -1,15 +1,27 @@
 import numpy as np
-from train import main
+import sys
+
+#sys.argv.append("--plan")
+
+if "--plan" in sys.argv:
+    import pandas as pd
+    data = []
+    def main(**kwargs):
+        print(kwargs)
+        data.append(kwargs)
+        pd.DataFrame(data).to_csv("jobs.csv")
+else:
+    from train import main
 
 
-for repeat in range(2):
+for repeat in range(3):
     model_type = "shallow_mlp"
     for dataset in ["mnist"]:
         for strength in [0.0, 0.1, 1.0]:
             for reg1value in np.arange(0.6, 5.1, 0.2):
                 reg = strength
                 main(dataset=dataset, model_type=model_type,
-                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat,
+                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat, min_x=5, max_x=1000,
                      output=f"logs/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}")
 
 for repeat in range(2):
@@ -19,7 +31,7 @@ for repeat in range(2):
             for reg1value in np.arange(0.6, 4.1, 0.2):
                 reg = strength
                 main(dataset=dataset, model_type=model_type,
-                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat,
+                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat, min_x=5, max_x=1000,
                      output=f"logs/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}")
 
 for repeat in range(3):
