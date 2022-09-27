cc7030edb7f62c97e8d56eabe2ed78b4fa542e7e
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-27 14:17:04 -0400

    accidently commited lines in run.py

--- training/run.py
+++ training/run.py
@@ -13,18 +13,6 @@ if "--plan" in sys.argv:
 else:
     from train import main
 
-if 1:
- for repeat in [0]:
-    model_type = "shallow_mlp"
-    for dataset in ["mnist"]:
-        for strength in [1.0]:#[0.0, 0.1, 1.0]:
-            for reg1value in [2.0]:#np.arange(0.6, 5.1, 0.2):
-                reg = strength
-                main(dataset=dataset, model_type=model_type,
-                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat, min_x=0, max_x=1000,
-                     output=f"logs3onlymseloss2/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}_B")
- exit()
-
 for repeat in range(3):
     model_type = "shallow_mlp"
     for dataset in ["mnist"]:
