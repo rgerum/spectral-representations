f4f69c5bee49b628ccdd54414c03a6ffacb74362
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-27 14:15:00 -0400

    changed epochs in run.py

--- training/run.py
+++ training/run.py
@@ -13,6 +13,17 @@ if "--plan" in sys.argv:
 else:
     from train import main
 
+if 1:
+ for repeat in [0]:
+    model_type = "shallow_mlp"
+    for dataset in ["mnist"]:
+        for strength in [1.0]:#[0.0, 0.1, 1.0]:
+            for reg1value in [2.0]:#np.arange(0.6, 5.1, 0.2):
+                reg = strength
+                main(dataset=dataset, model_type=model_type,
+                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat, min_x=0, max_x=1000,
+                     output=f"logs3onlymseloss2/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}_B")
+ exit()
 
 for repeat in range(3):
     model_type = "shallow_mlp"
@@ -21,17 +32,17 @@ for repeat in range(3):
             for reg1value in np.arange(0.6, 5.1, 0.2):
                 reg = strength
                 main(dataset=dataset, model_type=model_type,
-                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat, min_x=5, max_x=1000,
+                     reg_strength=strength, reg_target=reg1value, epochs=100, repeat=repeat, min_x=5, max_x=1000,
                      output=f"logs/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}")
 
-for repeat in range(2):
+for repeat in range(3):
     model_type = "deep_mlp"
     for dataset in ["mnist"]:
         for strength in [0.0, 0.1, 1.0]:
             for reg1value in np.arange(0.6, 4.1, 0.2):
                 reg = strength
                 main(dataset=dataset, model_type=model_type,
-                     reg_strength=strength, reg_target=reg1value, epochs=50, repeat=repeat, min_x=5, max_x=1000,
+                     reg_strength=strength, reg_target=reg1value, epochs=100, repeat=repeat, min_x=5, max_x=1000,
                      output=f"logs/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}")
 
 for repeat in range(3):
@@ -41,5 +52,5 @@ for repeat in range(3):
             for reg1value in np.arange(0.6, 4.1, 0.2):
                 reg = strength
                 main(dataset=dataset, model_type=model_type,
-                     reg_strength=strength, reg_target=reg1value, epochs=200, repeat=repeat,
+                     reg_strength=strength, reg_target=reg1value, epochs=100, repeat=repeat, min_x=5, max_x=1000,
                      output=f"logs/{model_type}/{dataset}/repeat-{repeat}_reg-strength-{strength}_reg-target-{reg1value:.1f}")
